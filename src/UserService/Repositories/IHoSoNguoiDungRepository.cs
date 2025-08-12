a1d85ba7f1da7b07c4dba104dc32a90e54943c62
fushiree <quannro2004@gmail.com>
2025-08-12 12:56:04 +0700

    Add User Service p2

---
+++ src/UserService/Repositories/IHoSoNguoiDungRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UserService.Models.Entities;
+
+namespace UserService.Repositories
+{
+    // Giao diện (interface) cho repository của HoSoNguoiDung
+    public interface IHoSoNguoiDungRepository
+    {
+        // Lấy tất cả hồ sơ người dùng
+        Task<IEnumerable<HoSoNguoiDung>> GetAllAsync();
+
+        // Lấy một hồ sơ người dùng theo ID
+        Task<HoSoNguoiDung> GetByIdAsync(string id);
+
+        // Thêm một hồ sơ người dùng mới
+        Task AddAsync(HoSoNguoiDung hoSo);
+
+        // Cập nhật thông tin một hồ sơ người dùng
+        Task UpdateAsync(HoSoNguoiDung hoSo);
+
+        // Xóa một hồ sơ người dùng theo ID
+        Task DeleteAsync(string id);
+    }
+}
