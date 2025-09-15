e3fd55cd3d46e210b19b235e02598ef4a379d47e
Khoi <kn24524@gmail.com>
2025-09-15 11:12:10 +0700

    Fix exam questions

--- src/ExamService/Profiles/MapsterConfig.cs
+++ src/ExamService/Profiles/MapsterConfig.cs
@@ -2,6 +2,7 @@ using Mapster;
 using ExamService.Models.DTOs;
 using ExamService.Models.Entities;
 using System.Text.Json;
+using System.Linq;
 
 namespace ExamService.Profiles;
 
@@ -28,7 +29,8 @@ public static class MapsterConfig
 
         // DeThi Mappings
         TypeAdapterConfig<DeThi, DeThiResponseDTO>.NewConfig()
-            .Map(dest => dest.SoLuongCauHoi, src => src.ExamQuestions.Count);
+            .Map(dest => dest.SoLuongCauHoi, src => src.ExamQuestions.Count)
+            .Map(dest => dest.CauHois, src => src.ExamQuestions.OrderBy(eq => eq.ThuTu).ToList());
 
         TypeAdapterConfig<DeThiRequestDTO, DeThi>.NewConfig()
             .IgnoreNullValues(true)
@@ -42,6 +44,23 @@ public static class MapsterConfig
             .Map(dest => dest.CauHoi, src => src.CauHoi)
             .Map(dest => dest.ThuTu, src => src.ThuTu)
             .Map(dest => dest.Diem, src => src.Diem);
+
+        // Mapping từ ExamQuestion sang CauHoiTrongDeThiDTO
+        TypeAdapterConfig<ExamQuestion, CauHoiTrongDeThiDTO>.NewConfig()
+            .Map(dest => dest.Id, src => src.CauHoi.Id)
+            .Map(dest => dest.NoiDung, src => src.CauHoi.NoiDung)
+            .Map(dest => dest.MonHoc, src => src.CauHoi.MonHoc)
+            .Map(dest => dest.LoaiCauHoi, src => src.CauHoi.LoaiCauHoi)
+            .Map(dest => dest.ChuDe, src => src.CauHoi.ChuDe)
+            .Map(dest => dest.DoKho, src => src.CauHoi.DoKho)
+            .Map(dest => dest.DapAnDung, src => src.CauHoi.DapAnDung)
+            .Map(dest => dest.GiaiThich, src => src.CauHoi.GiaiThich)
+            .Map(dest => dest.TaoLuc, src => src.CauHoi.TaoLuc)
+            .Map(dest => dest.CapNhatLuc, src => src.CauHoi.CapNhatLuc)
+            .Map(dest => dest.NguoiTaoId, src => src.CauHoi.NguoiTaoId)
+            .Map(dest => dest.LuaChons, src => src.CauHoi.LuaChons)
+            .Map(dest => dest.Diem, src => src.Diem)
+            .Map(dest => dest.ThuTu, src => src.ThuTu);
 
         // MauDeThi Mappings với JSON serialization
         TypeAdapterConfig<MauDeThi, MauDeThiResponseDTO>.NewConfig()
